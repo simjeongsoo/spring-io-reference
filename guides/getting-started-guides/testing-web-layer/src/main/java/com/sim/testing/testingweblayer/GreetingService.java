3d1cb69fb469f17d4465ccb9c3c8d220047d743b
simjeongsoo <simking95@naver.com>
2022-10-24 17:23:43 +0900

    [Service에 의존하는 Controller 테스트 ] - Service 생성

---
+++ guides/getting-started-guides/testing-web-layer/src/main/java/com/sim/testing/testingweblayer/GreetingService.java
@@ -0,0 +1,12 @@
+package com.sim.testing.testingweblayer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+
+    public String greeting() {
+        return "Service Layer";
+    }
+
+}
