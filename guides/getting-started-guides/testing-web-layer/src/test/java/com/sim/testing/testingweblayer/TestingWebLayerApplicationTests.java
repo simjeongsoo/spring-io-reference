1977c5fbadfb4004e4a6074f8a3c2dd64c5b3222
simjeongsoo <simking95@naver.com>
2022-10-24 17:12:42 +0900

    [Application 테스트] - MockMvc로 서버 없이 테스트

--- guides/getting-started-guides/testing-web-layer/src/test/java/com/sim/testing/testingweblayer/TestingWebLayerApplicationTests.java
+++ guides/getting-started-guides/testing-web-layer/src/test/java/com/sim/testing/testingweblayer/TestingWebLayerApplicationTests.java
@@ -1,15 +1,36 @@
 package com.sim.testing.testingweblayer;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TestingWebLayerApplicationTests {
-    //--sanity check test--//
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     void contextLoads() {
         //--Application Context를 시작할 수 없는 경우 실패하는 간단한 sanity check 테스트--//
+    }
+
+    @Test
+    void shouldReturnDefaultMessage() throws Exception{
+        //--MockMvc로 서버 없이 테스트 --//
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, World")));
     }
 
 }
