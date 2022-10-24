8461f29f608ac52857307118fdf03564752ca6d4
simjeongsoo <simking95@naver.com>
2022-10-24 16:54:48 +0900

    project init

---
+++ guides/getting-started-guides/testing-web-layer/src/main/java/com/sim/testing/testingweblayer/TestingWebLayerApplication.java
@@ -0,0 +1,13 @@
+package com.sim.testing.testingweblayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingWebLayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingWebLayerApplication.class, args);
+    }
+
+}
---
+++ guides/getting-started-guides/testing-web-layer/src/test/java/com/sim/testing/testingweblayer/TestingWebLayerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sim.testing.testingweblayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingWebLayerApplicationTests {
+    //--sanity check test--//
+
+    @Test
+    void contextLoads() {
+    }
+
+}
