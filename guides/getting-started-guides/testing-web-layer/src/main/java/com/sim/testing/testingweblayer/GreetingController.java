83f04800488a4bfa3e12a5ad7f607f7979d7377f
simjeongsoo <simking95@naver.com>
2022-10-24 17:23:35 +0900

    [Service에 의존하는 Controller 테스트 ] - Controller 생성

---
+++ guides/getting-started-guides/testing-web-layer/src/main/java/com/sim/testing/testingweblayer/GreetingController.java
@@ -0,0 +1,21 @@
+package com.sim.testing.testingweblayer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GreetingController {
+    //--GreetingService 에 의존하는 Controller 생성--//
+
+    private final GreetingService service;
+
+    public GreetingController(GreetingService service) {
+        this.service = service;
+    }
+
+    @RequestMapping("/greeting")
+    public @ResponseBody String greeting() {
+        return service.greeting();
+    }
+}
