9e70819497d87a54c698e801ec1fe9a1d29c271f
simjeongsoo <simking95@naver.com>
2022-10-24 17:14:31 +0900

    [Application 테스트] - @WebMvcTest 로 테스트 범위를 웹 레이어로만 좁히기

---
+++ guides/getting-started-guides/testing-web-layer/src/test/java/com/sim/testing/testingweblayer/WebLayerTest.java
@@ -0,0 +1,28 @@
+package com.sim.testing.testingweblayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class WebLayerTest {
+    //--@WebMvcTest 로 테스트 범위를 웹 레이어로만 좁히기--//
+
+    @Autowired
+    private MockMvc mockMvc;
+    
+    @Test
+    void shouldReturnDefaultMessage() throws Exception{
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, World")));
+    }
+}
