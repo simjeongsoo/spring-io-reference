a82645a56f6acb56e7721840cfeaf492d4cad406
simjeongsoo <simking95@naver.com>
2022-11-15 18:12:26 +0900

    인사말 표현 모델링 클래스

---
+++ guides/getting-started-guides/messaging-stomp-websocket/src/main/java/com/sim/messagingstompwebsocket/Greeting.java
@@ -0,0 +1,17 @@
+package com.sim.messagingstompwebsocket;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
