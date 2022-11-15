f81a36ba3ece4f6296a61127945b0cb2921a7a68
simjeongsoo <simking95@naver.com>
2022-11-15 17:33:02 +0900

    project init

---
+++ guides/getting-started-guides/messaging-stomp-websocket/src/test/java/com/sim/messagingstompwebsocket/MessagingStompWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sim.messagingstompwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingStompWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
