9bcef071e35e2288dcf99a8aacc4a013abbd0f52
simjeongsoo <simking95@naver.com>
2022-11-25 18:08:07 +0900

    project init

---
+++ guides/getting-started-guides/restservicecors/src/main/java/com/sim/restservicecors/RestservicecorsApplication.java
@@ -0,0 +1,13 @@
+package com.sim.restservicecors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestservicecorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestservicecorsApplication.class, args);
+    }
+
+}
