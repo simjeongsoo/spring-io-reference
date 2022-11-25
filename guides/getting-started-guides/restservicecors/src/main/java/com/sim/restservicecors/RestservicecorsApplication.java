70298852597e5071df4e70c8b2dcc69d2364b83c
simjeongsoo <simking95@naver.com>
2022-11-25 19:42:38 +0900

    [controller] - CORS 를 처리하는 방법을 구성하는 메서드를 추가

--- guides/getting-started-guides/restservicecors/src/main/java/com/sim/restservicecors/RestservicecorsApplication.java
+++ guides/getting-started-guides/restservicecors/src/main/java/com/sim/restservicecors/RestservicecorsApplication.java
@@ -2,6 +2,9 @@ package com.sim.restservicecors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestservicecorsApplication {
@@ -10,4 +13,17 @@ public class RestservicecorsApplication {
         SpringApplication.run(RestservicecorsApplication.class, args);
     }
 
+    /**
+     * method to configure how to handle cross-origin resource sharing
+     * */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/greeting-javaconfig")
+                        .allowedOrigins("http://localhost:8080");
+            }
+        };
+    }
 }
